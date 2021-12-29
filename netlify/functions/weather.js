dc6b4942e6295b25edac51338e10e57065d5e0c0
joongi <joongy007@naver.com>
2021-12-30 01:11:33 +0900

    stringify

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+const { json } = require("stream/consumers");
 
 const { API_KEY } = process.env
 exports.handler = async (event, context) => {
@@ -7,7 +8,7 @@ exports.handler = async (event, context) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     return await fetch(url).then(response => response.json()).then(data => ({
             statusCode: 200,
-            body: data,
+            body: json.stringify(data),
     }));
   };
 
