e4ca2433e2cc8aba49ca41c009143e498638b229
joongi <joongy007@naver.com>
2021-12-30 16:53:04 +0900

    version change

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,17 +1,20 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 const API_KEY = process.env.API_KEY
-export async function handler(event, context) {
+exports.handler = async function(event, context){
     const lat = event.queryStringParameters.lat;
     const lon = event.queryStringParameters.lon;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    return await fetch(url).then(response => response.json()).then(data => ({
+
+    const response = await fetch(url)
+    const data = response.json()
+
+    return {
         headers: {
             "content-type": "application/json; charset=utf-8",
         },
         statusCode: 200,
         body: JSON.stringify(data),
-    }));
+    }
 }
-
   
