a2ce2c67fcfe85eb6c0301cbcbbb1c4393d492eb
joongi <joongy007@naver.com>
2021-12-30 14:23:23 +0900

    add header

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -6,8 +6,11 @@ exports.handler = async (event, context) => {
     const lon = event.queryStringParameters.lon;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     return await fetch(url).then(response => response.json()).then(data => ({
-            statusCode: 200,
-            body: JSON.stringify(data),
+        headers: {
+            "content-type": "application/json; charset=utf-8",
+        },
+        statusCode: 200,
+        body: JSON.stringify(data),
     }));
   };
 
