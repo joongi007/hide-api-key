90210a42ea0b318de7766a31909553cea39e2c70
joongi <joongy007@naver.com>
2021-12-30 13:44:52 +0900

    Error correction.

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,14 +1,14 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 const { json } = require("stream/consumers");
 
-const { API_KEY } = process.env
+const API_KEY = process.env.API_KEY
 exports.handler = async (event, context) => {
     const lat = event.queryStringParameters.lat;
     const lon = event.queryStringParameters.lon;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     return await fetch(url).then(response => response.json()).then(data => ({
             statusCode: 200,
-            body: json.stringify(data),
+            body: JSON.stringify(data),
     }));
   };
 
