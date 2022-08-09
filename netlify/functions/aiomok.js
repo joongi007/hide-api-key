38ff569651b77aa1fb54c49c25e4efd9aac780e1
joongi <joongy007@naver.com>
2022-08-09 14:09:26 +0900

    mv model folder

--- netlify/functions/aiomok.js
+++ netlify/functions/aiomok.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.handler = async function(event, context){
     const API_KEY = process.env.API_KEY;
     const target = event.queryStringParameters.target;
-    const data = require(`../model/${target}/model.json`);
+    const data = require(`./model/${target}/model.json`);
 
     return {
         headers: {
