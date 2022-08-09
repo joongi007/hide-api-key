226a46c70b5a900f0a23637950575d87fda37e95
joongi <joongy007@naver.com>
2022-08-09 14:42:05 +0900

    mv model folder

--- netlify/functions/aiomok.js
+++ netlify/functions/aiomok.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context){
-    const API_KEY = process.env.API_KEY;
     const target = event.queryStringParameters.target;
-    const data = require(`./model/${target}/model.json`);
-
+    const url = `https://my-api-server.netlify.app/model/${target}/model.json`;
+    const response = await axios.get(url);
+    const data = await response.data;
+    //
     return {
         headers: {
             "Access-Control-Allow-Origin": "*",
             "content-type": "application/json; charset=utf-8",
         },
         statusCode: 200,
-        body: data,
+        body: JSON.stringify(data),
     };
 }
